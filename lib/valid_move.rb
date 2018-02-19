9fda2d543c7f3ba13595023bb8a1f76f2eeb4d00
stephenchou-ww <stephen.chou@wework.com>
2018-02-19 20:56:35 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,30 @@
 # code your #valid_move? method here
 
+def valid_move?(board, index)
+  if index >= 9 || index < 0
+    puts "not a position on the board!"
+    return false
+  else
+    occupancy = position_taken?(board, index)
+    if occupancy == true
+      return false
+    else 
+      return true
+    end
+  end
+end
+
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?(board, index)
+  if board[index] == " "
+    return false
+  elsif board[index] == nil
+    return false
+  elsif board[index] == ""
+    return false
+  else
+    return true
+  end
+end
